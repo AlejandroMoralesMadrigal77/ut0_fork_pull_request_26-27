77fe4de31294a421099347a15725db83f6b4d2bf
manu98137 <mro778094@iesvirreymorcillo.es>
2026-09-22 09:10:10 +0200

    HE AÑADIDO MI NOMBRE

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,7 @@ public class Main {
 
         System.out.println("Cristian Navarro López");
         System.out.println("Juan Carlos Alumbreros Fresneda");
+        System.out.println("Manuel Romero Delicado");
+
     }
 }
