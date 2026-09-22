33bdeb18a4af31862598604107dc071ef1ecd120
AlexMB1201 <amo629481@iesvirreymorcillo.es>
2026-09-22 09:38:29 +0200

    Nombre Alejandro

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("BIENVENIDO AL EJERCICIO FORK Y PULL REQUEST!");
-        System.out.println("____________________________________________");
+        System.out.println("___________________________________________");
 
         System.out.println("Cristian Navarro López");
         System.out.println("Juan Carlos Alumbreros Fresneda");
