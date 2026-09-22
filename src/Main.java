077c5626079a62755731a62b6f093e20f345d553
ccnl10 <ccnl10@iesvirreymorcillo.es>
2026-09-22 09:44:31 +0200

    modificación del formato de salida en main

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("BIENVENIDO AL EJERCICIO FORK Y PULL REQUEST!");
         System.out.println("___________________________________________");
-
+        System.out.println("Escribe tu nombre:");
         System.out.println("Cristian Navarro López");
         System.out.println("Juan Carlos Alumbreros Fresneda");
         System.out.println("Alejandro Montejano Bautista");
